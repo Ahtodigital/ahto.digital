d8010005cbcea6e92f86d98394dd0db9b922a336
Nelito <ngvjqp@gmail.com>
2018-04-22 02:32:33 +0000

    asdasda

---
+++ index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+   <?php include 'partials/head.php';?>
+  <body>
+    <?php include 'partials/header.php';?>
+
+    <section class="section-header fullscreen d-flex align-items-center bg-dark" style="padding-top: 72px;">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm text-center">
+            <h1 class="text-light mb40 big-title">Ajudamos você a criar a melhor versão do seu negócio</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="fullscreen d-flex align-items-center bg-primary" style="padding-top: 72px;">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <h1 class="text-light mb40 big-title">O que fazemos</h1>
+		<p class="text-light">		
+		Criamos soluções digitais para melhorar a experiência do seu cliente dentro de fora da internet.
+		</p><br>
+		<ul class="text-light">
+			<li>Sites e Plataformas</li>
+			<li>Conteúdo</li>
+			<li>Mídias pagas</li>
+			<li>Branding</li>
+		</ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="fullscreen d-flex align-items-center" style="padding-top: 72px;">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <h1 class="mb40 big-title">Quem somos</h1>
+                <p class="">          
+			Acreditamos que tudo possa ser melhorado, sempre.<br>
+			Por isso, criamos o Ahto Digital, um coletivo que busca evolução através da colaboração e experimentação constante.<br>
+			Queremos ajudar você a criar soluções que facilitem a vida das pessoas, para que todos possamos ter tempo e liberdade para <b>Ser e Fazer o que quiser.</b>
+                </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="bg-primary fullscreen d-flex align-items-center" style="padding-top: 72px;">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <h1 class="mb40 big-title">Quem somos</h1>
+                <p class="">          
+                        Acreditamos que tudo possa ser melhorado, sempre.<br>
+                        Por isso, criamos o Ahto Digital, um coletivo que busca evolução através da colaboração e experimentação constante.<br>
+                        Queremos ajudar você a criar soluções que facilitem a vida das pessoas, para que todos possamos ter tempo e liberdade para <b>Ser e Fazer o que$
+                </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <?php include 'partials/jsfooter.php';?>
+  </body>
+
+</html>
---
+++ partials/header.php
@@ -0,0 +1,9 @@
+<header class="ahto-header">
+    <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <a class="navbar-brand" href="#">
+        <img class="ahto-logo" src="img/ahto-digital.svg">
+      </a>
+    </nav>
+   </div>
+</header>
