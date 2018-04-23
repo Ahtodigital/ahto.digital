94dffbb7fa66b4fe89e7d0cb27577746b57155a3
Nelito Junior <ngvjqp@gmail.com>
2018-04-23 02:06:53 -0300

    V2
    Varias paradas

--- index.php
+++ index.php
@@ -1,60 +1,100 @@
 <!doctype html>
 <html lang="en">
-   <?php include 'partials/head.php';?>
+  <?php include 'partials/head.php';?>
+
   <body>
     <?php include 'partials/header.php';?>
-
-    <section class="section-header fullscreen d-flex align-items-center bg-dark" style="padding-top: 72px;">
-      <div class="container">
-        <div class="row">
+    <div class="bg1">
+      <section class="section-header fullscreen d-flex ">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm text-center">
+                <img class="ahto-logo" src="img/ahto.svg">
+            </div>
+          </div>
           <div class="col-sm text-center">
-            <h1 class="text-light mb40 big-title">Ajudamos você a criar a melhor versão do seu negócio</h1>
+            <img src="img/c1.svg" style="max-height:70vh"></img>
+          </div>
+          <div class="row" style="height:25vh;max-width:100vw;">
+            <div class="col-sm text-center">
+              <h1 class="text-light mb40 big-title">Ajudamos você a criar a melhor versão do seu negócio</h1>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="section-header fullscreen d-flex ">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm text-center mt24">
+              <h1 class="text-light mb40 big-title">Damos atenção ao que realmente importa</h1>
+            </div>
+          </div>
+          <div class="row  big-title text-center mb40">
+            <div class="col-sm text-grey">
+              <img src="img/c2.svg"></img>Experiências
+            </div>
+            <div class="col-sm text-light">
+              <img src="img/c3.svg"></img>Pessoas
+            </div>
+            <div class="col-sm text-grey">
+              <img src="img/c2.svg"></img>Resultados
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm text-center text-light mb40">
+              Pessoas estão no centro de todos os nosso processos
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+    <section class="section2 fullscreen d-flex ">
+      <div class="container">
+        <div class="row" style="padding: 5vh;">
+          <div class="col-sm text-center">
+            <h1 class="text-light mb40 big-title">Soluções</h1>
+          </div>
+        </div>
+        <div class="row text-light big-title text-center mb128">
+          <div class="col-sm text-grey">
+            <img src="img/c2.svg"></img>Branding
+          </div>
+          <div class="col-sm">
+            <img src="img/c1.svg"></img>Sites e Plataformas
+          </div>
+          <div class="col-sm text-grey">
+            <img src="img/c2.svg"></img>Conteúdo
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm text-center text-light mb40">
+            Criamos soluções digitais que entregam experiências notáveis para seus clientes e ótimos resultados para seu negócio
           </div>
         </div>
       </div>
     </section>
-    <section class="fullscreen d-flex align-items-center bg-primary" style="padding-top: 72px;">
+    <section class="fullscreen bg2 d-flex align-items-center section2">
       <div class="container">
         <div class="row">
           <div class="col-sm">
-            <h1 class="text-light mb40 big-title">O que fazemos</h1>
-		<p class="text-light">		
-		Criamos soluções digitais para melhorar a experiência do seu cliente dentro de fora da internet.
-		</p><br>
-		<ul class="text-light">
-			<li>Sites e Plataformas</li>
-			<li>Conteúdo</li>
-			<li>Mídias pagas</li>
-			<li>Branding</li>
-		</ul>
+            <img src="img/c1.svg"></img>
+          </div>
+          <div class="col-sm text-light big-title mb24">
+            Acreditamos que tudo possa ser melhorado, sempre.<br>
+
+            Por isso, por isso somos um coletivo que busca evolução constante através da colaboração e experimentação.<br>
+
+            Queremos ajudar você a criar soluções que facilitem a vida das pessoas, para que todos possamos ter tempo e liberdade para Ser e Fazer o que quiser.<br>
           </div>
         </div>
       </div>
     </section>
-    <section class="fullscreen d-flex align-items-center" style="padding-top: 72px;">
+    <section class="bg-primary fullscreen d-flex align-items-center">
       <div class="container">
-        <div class="row">
-          <div class="col-sm">
-            <h1 class="mb40 big-title">Quem somos</h1>
-                <p class="">          
-			Acreditamos que tudo possa ser melhorado, sempre.<br>
-			Por isso, criamos o Ahto Digital, um coletivo que busca evolução através da colaboração e experimentação constante.<br>
-			Queremos ajudar você a criar soluções que facilitem a vida das pessoas, para que todos possamos ter tempo e liberdade para <b>Ser e Fazer o que quiser.</b>
-                </p>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="bg-primary fullscreen d-flex align-items-center" style="padding-top: 72px;">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm">
-            <h1 class="mb40 big-title">Quem somos</h1>
-                <p class="">          
-                        Acreditamos que tudo possa ser melhorado, sempre.<br>
-                        Por isso, criamos o Ahto Digital, um coletivo que busca evolução através da colaboração e experimentação constante.<br>
-                        Queremos ajudar você a criar soluções que facilitem a vida das pessoas, para que todos possamos ter tempo e liberdade para <b>Ser e Fazer o que$
-                </p>
+        <div class="row ">
+          <div class="col-sm text-center">
+            <h1 class="mb40 big-title text-light">Contato</h1>
+            <input type="button" class="btn btn-ahto" style="text-transform: unset;" value="WhatsApp" onclick="location.href = 'https://api.whatsapp.com/send?phone=5548998586762';">
           </div>
         </div>
       </div>
