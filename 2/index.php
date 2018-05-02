994cb31d8467d2f39953fcfc7e7bc5770a0a4e95
Nelito Junior <ngvjqp@gmail.com>
2018-05-02 13:09:59 -0300

    lp incompleta

---
+++ 2/index.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="../css/lp.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet">
+    <title>AHTO</title>
+  </head>
+  <body>
+    <section id="sec1">
+      <div class="header">
+        <div class="logo">
+          <img src="../img/ahto-digital.svg">
+        </div>
+        <div>
+          ahto digital
+        </div>
+      </div>
+      <h1 class="">Somos um coletivo digital especializado em experiência do usuário</h1>
+      <div id="entenda">
+        <button>></button> Entenda Por Que
+      </div>
+    </section>
+    <section>
+      coisas top mercado digital
+    </section>
+    <section>
+        Ajudamos empreendedores a ganharem espaço no mercado digital
+    </section>
+    <section>
+      <h1>Identidade digital</h1>
+      Criamos uma cara para você.
+      <h1>Sites e Webstores</h1>
+      Fazemos seu E-commerce
+      <h1>Google</h1>
+      Colocamos você no google.
+      <button> Orçamento Grátis</button>
+    </section>
+    <section>
+      <h1>Materiais Gratuitos</h1>
+      <div>E-book
+        9 PASSOS ESSENCIAIS para você vender mais pela internet
+      </div>
+    </section>
+    <section>
+      <h1>Trabalhos</h1>
+      <div>
+        <div>1123
+        </div>
+        <div>425345
+        </div>
+      </div>
+      <div>
+        <div>345345
+        </div>
+        <div>456456
+        </div>
+      </div>
+      <div>
+        <div>456456
+        </div>
+        <div>3345345
+        </div>
+      </div>
+    </section>
+    <section>
+      <h1>Clientes</h1>
+      <div>
+        <div>1123
+        </div>
+        <div>425345
+        </div>
+      </div>
+      <div>
+        <div>345345
+        </div>
+        <div>456456
+        </div>
+      </div>
+    </section>
+    <section>
+      <h1>Tem alguma dúvida?</h1>
+      <div>
+        Entre em contato com a gente via WhatsApp. Respondemos na hora
+        <button>Chat via WhatsApp</button>
+      </div>
+    </section>
+    <section>
+      Centro, Florianópolis
+      48 991691304
+      ahto digital LOGO
+    </section>
+  </body>
+</html>
